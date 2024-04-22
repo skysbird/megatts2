b9cc5edb56a36b74c5cde4cfc345431200abaafb
skysbird <skysbird@gmail.com>
2024-04-22 10:58:53 +0800

    extract latent

--- models/gan2.py
+++ models/gan2.py
@@ -209,14 +209,10 @@ class VQGANTTS(nn.Module):
 
             content_encoder = instantiate_class(
                 args=(), init=G_config['init_args']['content_encoder'])
-            mel_decoder = instantiate_class(
-                args=(), init=G_config['init_args']['mel_decoder'])
-
 
             G_config['init_args']['mrte'] = mrte
             G_config['init_args']['vqpe'] = vqpe
             G_config['init_args']['content_encoder'] = content_encoder
-            G_config['init_args']['mel_decoder'] = mel_decoder
 
             G = instantiate_class(args=(), init=G_config)
 
